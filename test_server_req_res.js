3f5492ec74671a2c6ded486f109bf1ae2f90469e
gitgist37 <proventure37@gmail.com>
2024-01-17 16:40:06 +0530

    Added 2 files for writing and reading file with nodejs

--- test_server_req_res.js
+++ test_server_req_res.js
@@ -1,45 +1,91 @@
 const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer((req,res)=>
 {
+   
     const url = req.url;
-    
-    if(url==='/home')
+    const method = req.method;
+    if(url==='/')  // first time it encounters homepage
     {
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Test Server</title></head>');
-    res.write('<body><em>Welcome home</em></body>');
-    res.write('</html');
-    return res.end();
+        
+        fs.readFile('hello.txt',{encoding:'utf-8'},(err,value)=>
+            {
+                if(err)
+                {
+                console.error(err);
+                }
+                console.log("data fetched from file is",value);
+                res.write('<html>');
+                res.write('<head><title>Retrieved Data</title></head>');
+                res.write(`<body>${value}`);
+                res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+                res.write('</html');
+                console.log('Ye hamara last hai');
+                res.end();
+                });
     }
 
-    if(url==='/about')
+    if(url ==='/message' && method ==='POST') // after submission of data, php form trigger
     {
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Test Server</title></head>');
-    res.write('<body><em>Welcome to About Us page</em></body>');
-    res.write('</html');
-    return res.end();
-    }
+        
+        const body = [];
+        req.on('data',(traffic)=>
+        {
+            //console.log(traffic);
+            body.push(traffic);
+        });
 
-    if(url==='/node')
-    {
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Test Server</title></head>');
-    res.write('<body><b>Welcome to my Node Js Project</b></body>');
-    res.write('</html'); 
-    return res.end();
-    }
+        req.on('end',()=>
+        {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('hello.txt', message);
+            
+        });
 
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Test Server</title></head>');
-    res.write('<body>Welcome to my Node Js Project</body>');
-    res.write('</html');
-    res.end();
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+        // res.end();
+    }
+        // if(url==='/') // redirecting from action.php to localhost
+        // {
+        //     console.log("Yaha aya hai");
+        //     fs.readFile('hello.txt',{encoding:'utf-8'},(err,value)=>
+        //     {
+        //         if(err)
+        //         {
+        //         console.error(err);
+        //         }
+        //         console.log("data fetched from file is",value);
+        //         res.write('<html>');
+        //         res.write('<head><title>Retrieved Data</title></head>');
+        //         res.write(`<body>${value}`);
+        //         res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        //         res.write('</html');
+        //         res.end();
+        //         });
+               
+
+       
+        
+    //             
+    //             flag = false;
+    //             return res.end();
+        
+    //         });
+            
+    //     }
+    //     res.setHeader('Content-Type','text/html');
+    //     res.write('<html>');
+    //     res.write('<head><title>My First Page</title></head>');
+    //     res.write('<body><h1>Hello from my Node.Js Server!</h1></body>');
+    //     res.write('</html');
+    //     res.end();
+        
 });
-
+ 
+    
 server.listen(4000);
