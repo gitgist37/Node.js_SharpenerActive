a5ae1fed4f1649dd71e7c928361697070ae27e10
gitgist37 <proventure37@gmail.com>
2024-01-22 14:06:53 +0530

    Added js file after incorporating middleware fns along with app.listen

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fs = require('fs');
-fs.writeFileSync("hello.txt","Hello World!");
-console.log('Hello World!');
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next)=>
+{
+    console.log('Im in this middleware!');
+    next();
+});
+
+app.use((req,res,next)=>
+{
+    console.log('Im in this middleware too!');
+    res.send('<h1>Hello from Express!</h1>') // we can always override this line by manually setting header to a different value in the earlier line.
+});
+
+app.listen(4000);
