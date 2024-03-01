6b62bf456644437f7f8e95b769a1fe5799a8dbf1
gitgist37 <proventure37@gmail.com>
2024-03-01 12:32:37 +0530

    Made changes to the prexisting files from previous assgmt, added a cart.json file and a cart.js file

---
+++ pre_Made/models/cart.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+  );
+
+module.exports = class Cart {
+    static addProduct(id, productPrice)
+    {
+        fs.readFile(p, (err, fileContent)=>
+        {
+            let cart = {products: [], totalPrice: 0};
+            if(!err)
+            {
+                cart = JSON.parse(fileContent);
+            }
+
+            const ProductExistsIndex = cart.products.findIndex(prod=> prod.id===id);
+            const ProductExists = cart.products[ProductExistsIndex];
+            let newProduct;
+            if(ProductExists)
+            {
+                newProduct = {...ProductExists};
+                newProduct.qty = newProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[ProductExistsIndex] = newProduct;
+            }
+            else
+            {
+                newProduct = {id:id, qty:1};
+                cart.products = [...cart.products, newProduct];
+            }
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err)=>console.log(err));
+           
+        });
+    }
+}
